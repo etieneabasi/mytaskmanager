066468847d235132f5bbb31dc06871723ae34e2a
Etieneabasi Sunday Asuquo <abasi.asuquo@gmail.com>
2019-03-26 11:29:52 +0100

    master

--- src/migrations/2019_03_09_043835_create_tasks_table.php
+++ src/migrations/2019_03_09_043835_create_tasks_table.php
@@ -15,7 +15,7 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->unique();
+            $table->string('name');
             $table->unsignedInteger('category_id');
             $table->boolean('completed')->default(0);
             $table->timestamps();

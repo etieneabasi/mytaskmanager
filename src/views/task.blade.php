c55dffddf034a4199e45b8dcf5b2dbe023475595
Etieneabasi Sunday Asuquo <abasi.asuquo@gmail.com>
2019-03-28 16:56:55 +0100

    master

--- src/views/task.blade.php
+++ src/views/task.blade.php
@@ -184,7 +184,7 @@ Task
   @csrf
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalCenterTitle">Edit Task </h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>

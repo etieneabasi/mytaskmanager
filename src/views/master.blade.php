b3f47b2ff0df6f9b5e62c1410d719914c71ad1db
Etieneabasi Sunday Asuquo <abasi.asuquo@gmail.com>
2019-03-28 14:04:36 +0100

    master

--- src/views/master.blade.php
+++ src/views/master.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<body class="bg-primary">
+<body class="bg-light">
 @include('etieneabasi::nav')
 
 @yield('content')  
